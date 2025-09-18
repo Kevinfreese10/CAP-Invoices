90facefb6e0cf082d5af4e3a5f847ee6b718a0c5
Kevin Freese <kev@thinkestry.co.za>
2025-09-18 06:51:22 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "studio-2604127518-57889",
+  "appId": "1:248831476160:web:4ad085282b5fd36518c825",
+  "storageBucket": "studio-2604127518-57889.firebasestorage.app",
+  "apiKey": "AIzaSyD6-yfkYDj_ONK_tZdHhQy3RITU8F9zrU8",
+  "authDomain": "studio-2604127518-57889.firebaseapp.com",
+  "messagingSenderId": "248831476160"
+};
+
+// Initialize Firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+export { app as firebaseApp };
