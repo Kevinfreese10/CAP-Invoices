817ae5d3fe7c02c6997bf2bce93467f943222c11
Kevin Freese <kev@thinkestry.co.za>
2025-09-22 08:24:44 +0000

    Index has been enabled

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next';
+import { services } from '@/lib/data';
+import { blogPosts } from '@/lib/data';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  
+  // In a real app, you'd get this from an environment variable
+  const siteUrl = 'https://my-accountant-app.com'; 
+
+  const servicePages = services.map(service => ({
+    url: `${siteUrl}/services/${service.id}`,
+    lastModified: new Date(),
+  }));
+
+  const blogPostPages = blogPosts.map(post => ({
+    url: `${siteUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+  }));
+
+  const staticPages = [
+    { url: `${siteUrl}/`, lastModified: new Date() },
+    { url: `${siteUrl}/services`, lastModified: new Date() },
+    { url: `${siteUrl}/blog`, lastModified: new Date() },
+    { url: `${siteUrl}/support`, lastModified: new Date() },
+    { url: `${siteUrl}/contact`, lastModified: new Date() },
+    { url: `${siteUrl}/login`, lastModified: new Date() },
+    { url: `${siteUrl}/signup`, lastModified: new Date() },
+    { url: `${siteUrl}/reseller-signup`, lastModified: new Date() },
+  ];
+
+  return [
+    ...staticPages,
+    ...servicePages,
+    ...blogPostPages,
+  ];
+}
