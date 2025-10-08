2b78e7bac224a50b4ba8cd17861fcf5f7fcb0227
Kevin Freese <kev@thinkestry.co.za>
2025-10-08 10:16:07 +0000

    When ever a client is created, import the standard chart of accounts

--- src/app/admin/clients/page.tsx
+++ src/app/admin/clients/page.tsx
@@ -674,13 +674,13 @@ export default function AdminClientsPage() {
         payrollDueDate: data.payrollDueDate || null,
         role: 'client',
         source: 'Client Management',
+        chartOfAccounts: initialChartOfAccounts, // Always add chart of accounts
+        allocationRules: initialAllocationRules, // Always add allocation rules
     };
     
     if (createNumeraProfile) {
         clientData.hasNumeraProfile = true;
         clientData.source = 'Numera';
-        clientData.chartOfAccounts = initialChartOfAccounts;
-        clientData.allocationRules = initialAllocationRules;
     }
 
     try {
@@ -866,5 +866,6 @@ export default function AdminClientsPage() {
     </div>
   );
 }
+
 
 
