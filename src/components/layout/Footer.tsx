b4fb2c9710d8e91b687c5fb29eccf64e8a6569c2
Kevin Freese <kev@thinkestry.co.za>
2025-10-17 14:07:12 +0000

    Same with the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { Facebook, Twitter, Linkedin, MapPin, Mail, Phone } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="border-t bg-secondary/50">
+    <footer className="border-t bg-background">
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             
@@ -70,6 +70,7 @@ const Footer = () => {
 };
 
 export default Footer;
+
 
 
 
