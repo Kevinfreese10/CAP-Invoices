25dcd631ead2a8975280872bd2d19c9e917fd51e
Kevin Freese <kev@thinkestry.co.za>
2026-07-02 13:30:42 +0200

    Remove BlogContext and BlogProvider

--- src/contexts/ClientProviders.tsx
+++ src/contexts/ClientProviders.tsx
@@ -3,15 +3,12 @@
 import { CartProvider } from '@/contexts/CartContext';
 import { Toaster } from '@/components/ui/toaster';
 import { ReactNode } from 'react';
-import { BlogProvider } from './BlogContext';
 
 export default function ClientProviders({ children }: { children: ReactNode }) {
   return (
-    <BlogProvider>
-      <CartProvider>
-        {children}
-        <Toaster />
-      </CartProvider>
-    </BlogProvider>
+    <CartProvider>
+      {children}
+      <Toaster />
+    </CartProvider>
   );
 }
